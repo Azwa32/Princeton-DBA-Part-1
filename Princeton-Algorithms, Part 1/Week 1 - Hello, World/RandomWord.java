659bb5678e6ecb92539200a7938735c464f07a6f
Azwa32 <87596275+Azwa32@users.noreply.github.com>
2023-02-01 20:29:57 +0800

    Update RandomWord.java

---
+++ Princeton-Algorithms, Part 1/Week 1 - Hello, World/RandomWord.java
@@ -0,0 +1,12 @@
+public class RandomWord {
+    public static void main(String[] args) {
+        if (args.length == 2) {
+            System.out.println("Hello " + args[0] + " " + args[1]);
+            System.out.println("Goodbye " + args[1] + " " + args[0]);
+        }
+        else {
+            System.out.println("Please give 2 names");
+        }
+
+    }
+}
