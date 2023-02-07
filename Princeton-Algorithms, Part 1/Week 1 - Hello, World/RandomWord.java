095b2fabf3f3432768c3e2ceb5c85c2536672505
Azwa32 <87596275+Azwa32@users.noreply.github.com>
2023-02-07 22:32:49 +0800

    Update RandomWord.java

--- Princeton-Algorithms, Part 1/Week 1 - Hello, World/RandomWord.java
+++ Princeton-Algorithms, Part 1/Week 1 - Hello, World/RandomWord.java
@@ -3,11 +3,18 @@ public class RandomWord {
     public static void main(String[] args) {
 
         int i = 0;
+        table = []
         while (!StdIn.isEmpty()) {
             System.out.println("****");
-
+            String word = StdIn.readString();
+            //append to table word
+            
+            
             i++;
         }
+        l = length of table
+        StdRandom.bernoulli();
+        
         // check if there are more strings to read from args
 
 
