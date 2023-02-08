ae893fb7d58c3f8bb093397ac447f069e398b94e
Azwa32 <87596275+Azwa32@users.noreply.github.com>
2023-02-08 21:00:25 +0800

    Update HelloGoodbye.java

--- Princeton-Algorithms, Part 1/Week 1 - Hello, World/HelloGoodbye.java
+++ Princeton-Algorithms, Part 1/Week 1 - Hello, World/HelloGoodbye.java
@@ -1,3 +1,9 @@
+/* *****************************************************************************
+ *  Name:              Ada Lovelace
+ *  Coursera User ID:  123456
+ *  Last modified:     October 16, 1842
+ **************************************************************************** */
+
 public class HelloGoodbye {
     public static void main(String[] args) {
         if (args.length == 2) {
